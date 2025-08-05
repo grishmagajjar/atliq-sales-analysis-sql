94735edd3fc020ee8540662b650540bdd0ba089a
Grishma <52099500+grishmagajjar@users.noreply.github.com>
2025-08-05 20:13:39 +0530

    Create q4_top_prduct_market_cust.sql

---
+++ sql-queries/q4_top_prduct_market_cust.sql
@@ -0,0 +1,70 @@
+-- Genrate report for top market, products, and customer by net sale.
+
+-- we need to calculate net sale
+-- Net Sale = Gross price - Pre invoice discount - post invoice discount
+
+-- Creating the view `sales_preinv_discount` and store all the data in like a virtual table
+	CREATE  VIEW `sales_preinv_discount` AS
+	SELECT 
+    	    s.date, 
+            s.fiscal_year,
+            s.customer_code,
+            c.market,
+            s.product_code, 
+            p.product, 
+            p.variant, 
+            s.sold_quantity, 
+            g.gross_price as gross_price_per_item,
+            ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+            pre.pre_invoice_discount_pct
+	FROM fact_sales_monthly s
+	JOIN dim_customer c 
+		ON s.customer_code = c.customer_code
+	JOIN dim_product p
+        	ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+    		ON g.fiscal_year=s.fiscal_year
+    		AND g.product_code=s.product_code
+	JOIN fact_pre_invoice_deductions as pre
+        	ON pre.customer_code = s.customer_code AND
+    		pre.fiscal_year=s.fiscal_year
+
+-- Create a view for post invoice deductions: `sales_postinv_discount`
+	CREATE VIEW `sales_postinv_discount` AS
+	SELECT 
+    	    s.date, s.fiscal_year,
+            s.customer_code, s.market,
+            s.product_code, s.product, s.variant,
+            s.sold_quantity, s.gross_price_total,
+            s.pre_invoice_discount_pct,
+            (s.gross_price_total-s.pre_invoice_discount_pct*s.gross_price_total) as net_invoice_sales,
+            (po.discounts_pct+po.other_deductions_pct) as post_invoice_discount_pct
+	FROM sales_preinv_discount s
+	JOIN fact_post_invoice_deductions po
+		ON po.customer_code = s.customer_code AND
+   		po.product_code = s.product_code AND
+   		po.date = s.date;
+
+-- Finally creating the view `net_sales` which inbuiltly use/include all the previous created view and gives the final result
+	CREATE VIEW `net_sales` AS
+	SELECT 
+            *, 
+    	    net_invoice_sales*(1-post_invoice_discount_pct) as net_sales
+	FROM gdb0041.sales_postinv_discount;
+
+
+-- Stored proc to get top n markets by net sales for a given year
+	CREATE PROCEDURE `get_top_n_markets_by_net_sales`(
+        	in_fiscal_year INT,
+    		in_top_n INT
+	)
+	BEGIN
+        	SELECT 
+                     market, 
+                     round(sum(net_sales)/1000000,2) as net_sales_mln
+        	FROM net_sales
+        	where fiscal_year=in_fiscal_year
+        	group by market
+        	order by net_sales_mln desc
+        	limit in_top_n;
+	END
