42236136de9ed2f45cb4c17c54727df0c5159e63
Grishma <52099500+grishmagajjar@users.noreply.github.com>
2025-08-05 16:33:46 +0530

    Create q1_customer_sales_detail_with_gross_price.sql

---
+++ sql-queries/q1_customer_sales_detail_with_gross_price.sql
@@ -0,0 +1,34 @@
+-- Query 1: Generate a detailed sales report for customer 90002002 
+-- including product, quantity sold, unit price, and total gross revenue for fiscal year 2021.
+
+-- Created function get_fiscal_year
+	CREATE FUNCTION `get_fiscal_year`(calendar_date DATE) 
+	RETURNS int
+    	DETERMINISTIC
+	BEGIN
+        	DECLARE fiscal_year INT;
+        	SET fiscal_year = YEAR(DATE_ADD(calendar_date, INTERVAL 4 MONTH));
+        	RETURN fiscal_year;
+	END
+  -- Created function get_fiscal_year
+
+  -- main query
+	SELECT 
+    	    s.date, 
+            s.product_code, 
+            p.product, 
+            p.variant, 
+            s.sold_quantity, 
+            g.gross_price,
+            ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+            ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+            ON g.fiscal_year=get_fiscal_year(s.date)
+    	AND g.product_code=s.product_code
+	WHERE 
+    	    customer_code=90002002 AND 
+            get_fiscal_year(s.date)=2021     
+	LIMIT 1000000;
+  -- main query
