64f2532dd925d64779efe575d73d0bea703c2287
Grishma <52099500+grishmagajjar@users.noreply.github.com>
2025-08-05 19:08:47 +0530

    Update q1_customer_sales_detail_with_gross_price.sql

--- sql-queries/q1_customer_sales_detail_with_gross_price.sql
+++ sql-queries/q1_customer_sales_detail_with_gross_price.sql
@@ -32,3 +32,23 @@
             get_fiscal_year(s.date)=2021     
 	LIMIT 1000000;
   -- main query
+
+  -- if we want report for any other customer, then just create a stored procedure
+	CREATE PROCEDURE `get_monthly_gross_sales_for_customer`(
+        	in_customer_codes TEXT
+	)
+	BEGIN
+        	SELECT 
+                    s.date, 
+                    SUM(ROUND(s.sold_quantity*g.gross_price,2)) as monthly_sales
+        	FROM fact_sales_monthly s
+        	JOIN fact_gross_price g
+               	    ON g.fiscal_year=get_fiscal_year(s.date)
+                    AND g.product_code=s.product_code
+        	WHERE 
+                    FIND_IN_SET(s.customer_code, in_customer_codes) > 0
+        	GROUP BY s.date
+        	ORDER BY s.date DESC;
+	END
+  -- if we want report for any other customer, then just create a stored procedure
+
